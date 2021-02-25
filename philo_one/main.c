3cc080590f93856d0881a0fbf4c75960edede047
Evrard Dessain <edessain@shi-r12-p1.s19.be>
2021-02-25 13:32:39 +0100

    norminette

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,48 +6,47 @@
 /*   By: edessain <edessain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 14:47:32 by edessain          #+#    #+#             */
-/*   Updated: 2021/02/19 12:55:48 by edessain         ###   ########.fr       */
+/*   Updated: 2021/02/25 13:29:07 by edessain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "./include/philo_one.h"
+#include "./include/philo_one.h"
 
-int creat_thread(t_data *one)
+int		creat_thread(t_data *one)
 {
-    int         i;
+	int			i;
 
-    i = 0;
-    pthread_mutex_init(&one->global, NULL);
-    pthread_mutex_init(&one->dead, NULL);
-    pthread_mutex_lock(&one->dead);
-    while (i < one->number_of_philo)
-    {
-        pthread_mutex_init(&one->mutex[i], NULL);
-        one->name[i] = i;
-        one->last_eat[i] = 0;
-        one->iter[i] = 0;
-        i++;
-    }
-    i = 0;
-    while (i < one->number_of_philo)
-    {
-        pthread_create(&one->philo[i], NULL, &routine, &one->name[i]);
-        i++;
-    }
+	i = 0;
+	pthread_mutex_init(&one->global, NULL);
+	pthread_mutex_init(&one->dead, NULL);
+	pthread_mutex_lock(&one->dead);
+	while (i < one->number_of_philo)
+	{
+		pthread_mutex_init(&one->mutex[i], NULL);
+		one->name[i] = i;
+		one->last_eat[i] = 0;
+		one->iter[i] = 0;
+		i++;
+	}
+	i = 0;
+	while (i < one->number_of_philo)
+	{
+		pthread_create(&one->philo[i], NULL, &routine, &one->name[i]);
+		i++;
+	}
 	pthread_create(&one->check_dead, NULL, &check_time, NULL);
-    pthread_mutex_lock(&one->dead);
-    destroy_mutex(one);
-    write(1, "ok\n", 3);
+	pthread_mutex_lock(&one->dead);
+	destroy_mutex(one);
 	return (1);
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    t_data       one;
+	t_data		one;
 
-    init_struct(&one);
-    fill_struct(&one, argc, argv);
-    *static_struct() = &one;
-    creat_thread(&one);
-    return (0);
+	init_struct(&one);
+	fill_struct(&one, argc, argv);
+	*static_struct() = &one;
+	creat_thread(&one);
+	return (0);
 }
--- philo_one/srcs/ft_action.c
+++ philo_one/srcs/ft_action.c
@@ -6,7 +6,7 @@
 /*   By: edessain <edessain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 18:04:21 by edessain          #+#    #+#             */
-/*   Updated: 2021/02/23 16:16:59 by edessain         ###   ########.fr       */
+/*   Updated: 2021/02/25 13:32:04 by edessain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,14 @@ void	sleeping(t_data *one, int i)
 		ft_print_str(time, i + 1, ft_strdup(" is sleeping\n"));
 		ft_sleep(one, one->time_to_sleep);
 	}
-
 }
-int 	check_iter(t_data *one, int i)
+
+int		check_iter(t_data *one, int i)
 {
 	if (one->iter[i] == one->number_of_time)
 	{
 		one->statut = i;
-		pthread_mutex_lock(&one->global);	
+		pthread_mutex_lock(&one->global);
 		pthread_mutex_unlock(&one->dead);
 		return (-1);
 	}
@@ -98,7 +98,7 @@ void	*routine(void *arg)
 	while (one->statut == -1)
 	{
 		if (one->number_of_time != -1 && check_iter(one, i) != 0)
-			break;
+			break ;
 		if (one->statut == -1)
 			ft_print_think(get_time(one), i + 1);
 		pthread_mutex_lock(&one->mutex[fork_left]);
