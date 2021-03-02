fd963090e2db96cd66af2b3dbf4142dccf7cc672
Evrard Dessain <edessain@mi-c2-p8.s19.be>
2021-03-02 15:34:44 +0100

    philo 3 begining

--- philo_three/main.c
+++ philo_three/main.c
@@ -1,6 +1,20 @@
 #include "../include/philo_three.h"
 
 
+int     creat_frok(t_data *three)
+{
+    int     i;
+    int     pid;
+
+    i = 0;
+    
+    while (i < three->number_of_philo)
+    {
+
+    }
+
+}
+
 int     main(int argc, char **argv)
 {
 	init_struct(&three);
